122da74c8ecc71fecc5a3eb61b266abcff517a79
ahmadrules <ahmattomaar@gmail.com>
2025-12-16 06:56:59 +0100

    hid api

--- python/app.py
+++ python/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+import trafikLab
 
 app = FastAPI()
 
--- python/trafikLab.py
+++ python/trafikLab.py
@@ -1,28 +1,29 @@
 import json
-from google.transit import gtfs_realtime_pb2
+from os import getenv
+
 import requests
 
-#feed = gtfs_realtime_pb2.FeedMessage()
-#response = requests.get("https://opendata.samtrafiken.se/gtfs-rt/skane/TripUpdates.pb?key=362642b3454f4932886d717537c14846")
-
-#feed.ParseFromString(response.content)
-#print(feed.entity[0].trip_update.stop_time_update[0].arrival.delay)
-
-resAPI = "b33ebb15-55e2-4b01-a3ed-391ef83278cd"
+resAPI = getenv("API_KEY")
 
 def fetchStopId(name):
     response = requests.get("https://api.resrobot.se/v2.1/location.name?input=" + name + "&maxNo=1&format=json&accessId=" + resAPI)
     data = json.loads(response.content)
     return data['stopLocationOrCoordLocation'][0]['StopLocation']['extId']
 
-def findTripId(stop1, stop2):
+def findTrip(stop1, stop2):
     response = requests.get("https://api.resrobot.se/v2.1/trip?format=json&originId="
                              + str(stop1) + "&destId="
                              + str(stop2) +
                              "&numF=1&format=json&passlist=0&showPassingPoints=true&accessId="
                              + resAPI)
     data = json.loads(response.content)
+    return data
 
+
+stopId1 = fetchStopId("MalmöRamelsVäg")
+stopId2 = fetchStopId("Lund")
+
+print(findTrip(stopId1, stopId2))
 
 # for entity in feed.entity:
  #   if entity.HasField('trip_update'):
