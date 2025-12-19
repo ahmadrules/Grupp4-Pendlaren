21168264e81393203beca3b8de04693fe2bc3703
ahmadrules <ahmattomaar@gmail.com>
2025-12-19 09:37:30 +0100

    t

--- python/app.py
+++ python/app.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from starlette.responses import Response
+from starlette.templating import Jinja2Templates
+
 import trafikLab
 
 app = FastAPI()
 
 app.mount("/", StaticFiles(directory="python/static", html=True), name="static")
+templates = Jinja2Templates(directory="python/templates")
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/?from-stop={fromStop}&to-stop={toStop}")
+async def getTrip(fromStop: str, toStop: str):
+    print(fromStop, toStop)
+    return trafikLab.findTrip(fromStop, toStop)
--- python/trafikLab.py
+++ python/trafikLab.py
@@ -6,24 +6,38 @@ import requests
 resAPI = getenv("API_KEY")
 
 def fetchStopId(name):
-    response = requests.get("https://api.resrobot.se/v2.1/location.name?input=" + name + "&maxNo=1&format=json&accessId=" + resAPI)
+    response = requests.get("https://api.resrobot.se/v2.1/location.name?input=" + str(name) + "&maxNo=1&format=json&accessId=" + str(resAPI))
     data = json.loads(response.content)
     return data['stopLocationOrCoordLocation'][0]['StopLocation']['extId']
 
 def findTrip(stop1, stop2):
-    response = requests.get("https://api.resrobot.se/v2.1/trip?format=json&originId="
-                             + str(stop1) + "&destId="
-                             + str(stop2) +
-                             "&numF=1&format=json&passlist=0&showPassingPoints=true&accessId="
-                             + resAPI)
-    data = json.loads(response.content)
-    return data
+    stopId1 = fetchStopId(stop1)
+    stopId2 = fetchStopId(stop2)
+    if stopId1 != -1 and stopId2 != -1:
+        response = requests.get("https://api.resrobot.se/v2.1/trip?format=json&originId="
+                                + str(stopId1) + "&destId="
+                                + str(stopId2) +
+                                "&numF=1&format=json&passlist=0&showPassingPoints=true&accessId="
+                                + str(resAPI))
+        data = json.loads(response.content)
+        return readTrip(data)
 
 
-stopId1 = fetchStopId("MalmöRamelsVäg")
-stopId2 = fetchStopId("Lund")
+def readTrip(jsonData):
+    legs = jsonData['Trip'][0]['LegList']['Leg']
+    legsOut = []
 
-print(findTrip(stopId1, stopId2))
+    for leg in legs:
+        currentLeg = {
+            "fromStation": leg['Origin']['name'],
+            "fromTime": leg['Origin']['time'],
+            "toStation": leg['Destination']['name'],
+            "toTime": leg['Destination']['time'],
+        }
+        legsOut.append(currentLeg)
+
+    return json.dumps(legsOut)
+
 
 # for entity in feed.entity:
  #   if entity.HasField('trip_update'):
