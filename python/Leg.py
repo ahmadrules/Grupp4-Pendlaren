3434242ba50a0fc3c29901fbf059fbef69988275
ahmadrules <ahmattomaar@gmail.com>
2025-12-26 12:17:38 +0100

    Revert "t"
    This reverts commit 3b235a8e3de833e7dc0301d6ab460d6b68a95c2b.

--- python/Leg.py
+++
@@ -1,6 +0,0 @@
-class Leg:
-    def __init__(self, originName, originTime, destinationName, destinationTime):
-        self.originName = originName
-        self.originTime = originTime
-        self.destinationName = destinationName
-        self.destinationTime = destinationTime
