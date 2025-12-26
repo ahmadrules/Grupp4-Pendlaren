3b235a8e3de833e7dc0301d6ab460d6b68a95c2b
ahmadrules <ahmattomaar@gmail.com>
2025-12-26 12:15:39 +0100

    t

---
+++ python/Leg.py
@@ -0,0 +1,6 @@
+class Leg:
+    def __init__(self, originName, originTime, destinationName, destinationTime):
+        self.originName = originName
+        self.originTime = originTime
+        self.destinationName = destinationName
+        self.destinationTime = destinationTime
--- python/trafikLab.py
+++ python/trafikLab.py
@@ -1,16 +1,25 @@
 import json
 from os import getenv
+import Leg
 
 import requests
 
 resAPI = getenv("API_KEY")
 
 def fetchStopId(name):
+<<<<<<< Updated upstream
     response = requests.get("https://api.resrobot.se/v2.1/location.name?input=" + str(name) + "&maxNo=1&format=json&accessId=" + str(resAPI))
+=======
+    response = requests.get("https://api.resrobot.se/v2.1/location.name?input="
+                            + name
+                            + "&maxNo=1&format=json&accessId="
+                            + resAPI)
+>>>>>>> Stashed changes
     data = json.loads(response.content)
     return data['stopLocationOrCoordLocation'][0]['StopLocation']['extId']
 
 def findTrip(stop1, stop2):
+<<<<<<< Updated upstream
     stopId1 = fetchStopId(stop1)
     stopId2 = fetchStopId(stop2)
     if stopId1 != -1 and stopId2 != -1:
@@ -25,6 +34,41 @@ def findTrip(stop1, stop2):
 def readTrip(jsonData):
     legs = jsonData['Trip'][0]['LegList']['Leg']
     legsOut = []
+=======
+    response = requests.get("https://api.resrobot.se/v2.1/trip?format=json&originId="
+                             + str(stop1)
+                             + "&destId="
+                             + str(stop2) +
+                             "&numF=1&format=json&passlist=0&showPassingPoints=true&accessId="
+                             + resAPI)
+    data = json.loads(response.content)
+    getTripTime(data)
+    getTripLegs(data)
+    return data
+
+def getTripTime(data):
+    time = data['Trip'][0]['Origin']['time']
+    time2 = data['Trip'][0]['Destination']['time']
+    print(time)
+    print(time2)
+    h1, m1, s1 = time.split(':')
+    h2, m2, s2 = time2.split(':')
+    time1Sec = int(h1) * 3600 + int(m1) * 60 + int(s1)
+    time2Sec = int(h2) * 3600 + int(m2) * 60 + int(s2)
+    print((time2Sec - time1Sec)/60)
+    return time2Sec - time1Sec
+
+def getTripLegs(data):
+    legs = []
+    datalen = data['Trip'][0]['LegList']['Leg']
+    for x in datalen:
+        originName = x['Origin']['name']
+        originTime = x['Origin']['time']
+        destinationName = x['Destination']['name']
+        destinationTime = x['Destination']['time']
+        leg = Leg.Leg(originName, originTime, destinationName, destinationTime)
+        print(leg.originName)
+>>>>>>> Stashed changes
 
     for leg in legs:
         currentLeg = {
