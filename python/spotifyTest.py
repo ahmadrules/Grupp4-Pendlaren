a011ecdc65443777adc23f4b4c2dc36f481f9689
ahmadrules <ahmattomaar@gmail.com>
2025-12-28 23:37:01 +0100

    Update spotifyTest.py

--- python/spotifyTest.py
+++ python/spotifyTest.py
@@ -4,7 +4,7 @@ import requests
 
 async def getAuth():
     url = "https://accounts.spotify.com/api/token"
-    data = {'grant_type' : 'client_credentials', 'client_id' : 'e6740bb7feb04f329db3f1cf4ebffefe', 'client_secret' : 'c33de715a721461c904836a34424c710'}
+    data = {'grant_type' : 'client_credentials', 'client_id' : '', 'client_secret' : ''}
     r = requests.post(url, data=data)
     print(r.text)
     auth = r.json()['access_token']
